64e2d57ad4b99566e35e0beee579df7fd966d9d5
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-07-06 15:44:00 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello from src/index.js!");
+// const results = document.querySelector("#results");
+// const searchMovie = (keyword) => {
+//   const apiUrl = `http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
+//   fetch(apiUrl)
+//     .then(response => response.json())
+//     .then((data) => {
+//       data.Search.forEach((movie) => {
+//         console.log(movie);
+//         const film = `<li>
+//           <img src="${movie.Poster}" alt="" />
+//           <p>${movie.Title}</p>
+//         </li>`;
+//         results.insertAdjacentHTML("beforeend", film);
+//       });
+//     });
+// };
+// const finalSearch = document.querySelector("#search-movies");
+// finalSearch.addEventListener("submit", (event) => {
+//   results.innerHTML = "";
+//   event.preventDefault();
+//   const title = document.querySelector("#keyword").value;
+//   searchMovie(title);
+// });
+const searchAlgoliaPlaces = (event) => {
+  fetch("https://places-dsn.algolia.net/1/places/query", {
+    method: "POST",
+    body: JSON.stringify({ query: event.currentTarget.value })
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.hits); // Look at local_names.default
+    });
+};
+const value = document.querySelector("#keyword");
+value.addEventListener("keyup", searchAlgoliaPlaces);
